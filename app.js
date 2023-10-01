69cfbba4ab7c79594338fafdb1e02bdb08fe436e
Alejandro Sanchez <alejadro.movil@hotmail.com>
2023-10-01 10:20:06 -0300

    Es el segundo comit y creo que va con errores

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-// MENU NAVEGADOR //
-var menu_visible = false
+// Menu Navegador //
+
+var menu_visible = false;
 let menu = document.getElementById("nav");
 function mostrarOcultarMenu(){
-   if(menu_visible = false){
+   if(menu_visible==false){//si esta oculto
         menu.style.display = "block";
         menu_visible = true;
    }
    else{
-      menu.style.display = "none";
+      menu.style.display = 'none';
       menu_visible = false;
    }
 }
+// Ocultar menu navegador una vez elegida la opcion//
 
 let links = document.querySelectorAll("nav a");
 for(var x = 0; x <links.length;x++){
-   links(x).onclick = function(){
+   links[x].onclick = function(){
       menu.style.display = "none";
       menu_visible = "false";
    }
+}
 
-} 
+function mostrar(){
+   document.getElementById("audio").style.display = "block";
+}
+
+function ocultar(){
+   document.getElementById("audio").style.display = "none";
+}
